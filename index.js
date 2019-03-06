d2e9e7c6999b7ec05bcc871cc4e8c113dd9b3060
Bao Pham <ryanvuepham@gmail.com>
2019-03-05 21:23:53 -0600

    added errorhandler middleware

--- index.js
+++ index.js
@@ -7,11 +7,23 @@ const Userdb = require('./data/helpers/userDb')
 
 const parser = express.json()
 
-
+// middleware
 server.use(parser, cors() ) 
 
 const port = 7000
 
+
+server.use(express.json(), cors() );
+
+const validate = (req, res, next) => {
+  if(!req.body.name) {
+    next('u1');
+  } else if (req.body.name.length > 128) {   
+    next('u2');
+  } else {
+    next();
+  }
+}
 
 const Uppercase = (req, res, next) => {
   if(!req.body.name) {
@@ -70,27 +82,40 @@ server.delete('/:id', (req, res) => {
 })
 
 
- server.put('/:id', Uppercase, (req, res) => {
-  if(!req.body.name) {
-    res.status(400).json({ error: "Need a new user to update."})
-  } else if(req.body.name.length > 129) {
-    res.status(400).json({ error: "Too many characters in name. Needs to be less than 129."})
-
-  } else {
-    const { id } = req.params;
-    const { name } = req.body;
-    const newUser = { name: req.name }
-    Userdb.update(id, newUser)
-      .then(response => {
-        if(!response) {
-          res.status(400).json({ error: "Wrong id was given"})
-        } else {
-          res.status(200).json({message: "Success!"})
-        }
-      })
-      .catch(err => res.status(400).json({ error: err }))
+ server.put('/:id', validate, Uppercase, (req, res) => {
+   const { id } = req.params;
+   const { name } = req.body;
+   const newUser = { name: req.name }
+   Userdb.update(id, newUser)
+     .then(response => {
+       if(!response) {
+         res.status(400).json({ error: "Wrong id was given"})
+       } else {
+         res.status(200).json({message: "Success!"})
+       }
+     })
+     .catch(err => res.status(400).json({ error: err }))
+  })
+     
+  const errorHandle = (err, req, res, next) => {
+    const errors = {
+      u1: {
+        title: "Require Name",
+        description: "Name is required.",
+        httpCode: 400
+      },
+      u2: {
+        title: "Require Name",
+        description: "Name is too long, needs to be less than 128 characters.",
+        httpCode: 400
+      }
+    }
+  
+    const error = errors[err];
+    res.status(error.httpCode).json(error);
   }
-})
+  
+  server.use(errorHandle);
 
 
 server.listen(port, () => {
